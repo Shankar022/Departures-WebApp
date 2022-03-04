ded852b52eaf204a7becbad1fb02d9ad63238d7e
Shannkar022 <shankarweb22@gmail.com>
2022-03-04 19:23:48 +0530

    created POST route ''/api/v1/tours'

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ const fs = require('fs');
 
 const app = express();
 
-// app.get('/',(req,res)=>{
-//   res.status(200).json({message:'from server side!'});
-// })
-//
-// app.post('/',(req,res)=>{
-//   res.send('hello POST !')
-// })
+//middleware
+app.use(express.json());
+
 
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -22,6 +18,21 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours: tours
         }
+    })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    //to get all data in the req object from the request we should use middleware.
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
     })
 })
 
