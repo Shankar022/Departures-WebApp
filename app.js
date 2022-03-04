aba6b2e093bf6ad1982a29acb796e76926660617
Shannkar022 <shankarweb22@gmail.com>
2022-03-04 19:49:25 +0530

    created patch/update route by id - '/api/v1/tours/:id'

--- app.js
+++ app.js
@@ -59,6 +59,23 @@ app.get('/api/v1/tours/:id', (req, res) => {
     })
 })
 
+app.patch('/api/v1/tours/:id',(req,res)=>{
+    const id = req.params.id * 1;
+    if (id > tours.length - 1) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+
+    res.status(200).json({
+        status:'success',
+        data:{
+            tour:'<Updated tour here !>'
+        }
+    })
+})
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`);
