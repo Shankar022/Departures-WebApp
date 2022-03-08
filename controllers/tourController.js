97e6f088e690c1c8fe6b22b24394dbbfd143a747
Shannkar022 <shankarweb22@gmail.com>
2022-03-08 13:57:42 +0530

    findByIdAndDelete() in mongoose ✔

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -80,11 +80,17 @@ exports.updateTour = async (req, res) => {
 };
 
 
-exports.deleteTour = (req, res) => {
-    const id = req.params.id * 1;
-
-    res.status(204).json({
-        status: 'success',
-        data: null,
-    });
+exports.deleteTour = async (req, res) => {
+    try{
+        await Tour.findByIdAndDelete(req.params.id)
+        res.status(204).json({
+            status: 'success',
+            data: null
+        });
+    }catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
 };
