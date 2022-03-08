6e777d7f8eb5342a9ef1991217aee8e57ee0fd4a
Shannkar022 <shankarweb22@gmail.com>
2022-03-08 11:05:14 +0530

    Connect to mongoDB ✌

--- server.js
+++ server.js
@@ -2,6 +2,42 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+const mongoose = require('mongoose');
+
+const mongoDB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(mongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    //console.log(conn.connections);
+    console.log('DB connection successful !');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique:true
+  },
+  rating: {
+    type:Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+  
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
