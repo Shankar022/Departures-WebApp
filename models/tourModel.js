996cf90815f06310559e61577aa98d20715aa67b
Shannkar022 <shankarweb22@gmail.com>
2022-03-25 22:18:24 +0530

    Data Validation: Built-In & Custom Validators ✔

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema({
 		name: {
@@ -7,6 +8,9 @@ const tourSchema = new mongoose.Schema({
 			required: [true, 'A tour must have a name'],
 			unique: true,
 			trim: true,
+			maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+			minlength: [10, 'A tour name must have more or equal then 10 characters'],
+			// validate: [validator.isAlpha, 'Tour name must only contain characters']
 		},
 		slug: String,
 		duration: {
@@ -20,10 +24,16 @@ const tourSchema = new mongoose.Schema({
 		difficulty: {
 			type: String,
 			required: [true, 'A tour must have a difficulty'],
+			enum: {
+				values: ['easy', 'medium', 'difficult'],
+				message: 'Difficulty is either: easy, medium, difficult'
+			}
 		},
 		ratingsAverage: {
 			type: Number,
 			default: 4.5,
+			min: [1, 'Rating must be above 1.0'],
+			max: [5, 'Rating must be below 5.0']
 		},
 		ratingsQuantity: {
 			type: Number,
@@ -34,7 +44,14 @@ const tourSchema = new mongoose.Schema({
 			required: [true, 'A tour must have a price']
 		},
 		priceDiscount: {
-			type: Number
+			type: Number,
+			validate: {
+				validator: function (val) {
+					// this only points to current doc on NEW document creation
+					return val < this.price;
+				},
+				message: 'Discount price ({VALUE}) should be below regular price'
+			}
 		},
 		summary: {
 			type: String,
@@ -73,7 +90,7 @@ tourSchema.virtual('durationWeeks').get(function () {
 
 //4 types of middleware: document middleware, model middleware, aggregate middleware, and query middleware.
 
-//DOCUMENT MIDDLEWARE: runs before .save(_ and .create()
+//DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function (next) {
 	this.slug = slugify(this.name, {lower: true});
 	next();
